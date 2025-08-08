d9040d83eeff597de90e45f37d7a7a4a2df2d5bd
Ali Qambari <usend@live.com>
2025-08-08 14:19:40 +0330

    Fix: change filter field name to match the model

--- blog/posts/api/v2/views.py
+++ blog/posts/api/v2/views.py
@@ -6,4 +6,4 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-updated_at')
     serializer_class = PostSerializer
     #using filter by query params on categories :GET /api/v2/posts/?category=2
-    filterset_fields = ['category']
+    filterset_fields = ['categories']
