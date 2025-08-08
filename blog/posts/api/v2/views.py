b395cc424769d326cbba8d71a120408b0a53fc41
Ali Qambari <usend@live.com>
2025-08-08 13:44:21 +0330

    Feat: add ability to filter based on categories for api_v2

--- blog/posts/api/v2/views.py
+++ blog/posts/api/v2/views.py
@@ -5,3 +5,5 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-updated_at')
     serializer_class = PostSerializer
+    #using filter by query params on categories :GET /api/v2/posts/?category=2
+    filterset_fields = ['category']
