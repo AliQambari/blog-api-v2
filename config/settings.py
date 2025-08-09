869ce8d4681e28dd57291e20500fe849ce9b5acd
Ali Qambari <usend@live.com>
2025-08-09 23:22:44 +0330

    Fix: DB migration to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -104,11 +104,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'user1',
+        'PASSWORD': 'pass1',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
